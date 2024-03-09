b52354d18f8814202b5fe359df748e1b6bd4b9e3
drAJaysvit <109869929+drAJaysvit@users.noreply.github.com>
2024-03-10 02:13:03 +0530

    Update new_rest.py

--- Backend/new_rest.py
+++ Backend/new_rest.py
@@ -536,6 +536,6 @@ def leave_group():
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    app.run(debug=True,host='0.0.0.0')
     # app.run(host='192.168.31.55', port=5000)
-    app.run()
+    # app.run()
